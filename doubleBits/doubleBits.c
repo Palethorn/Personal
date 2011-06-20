0a3c1a60a54c30b5494912729402fba0157c457c
Palethorn <wizzard405@yahoo.com>
2011-06-20 10:44:53 +0000

    Added GPL licence

--- doubleBits/doubleBits.c
+++ doubleBits/doubleBits.c
@@ -1,3 +1,20 @@
+/*
+    Program for doubling bits of a given variable
+    Copyright (C) 2011  David Ćavar
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "includes.h"
 /*
 						EXPLANATION
@@ -10,9 +27,7 @@ If we have 1101 then the value of given paramerer in recursion would go like thi
 1
 then in return doubling the last bit makes it 11 and shifting it we get 1100. after we glue the next doubled bit
 to it we get 1111 and so on.
-Algorithm works for any given word size assuming the data can fit without loss. You may need to use casting for this.
-
-Tested using gcc 4.4.3 on Debian Squeeze
+Algorithm works for any given word size assuming the data can fit inside memory location without loss. You may need to use casting for this.
 */
 int doubleBits(int a){
 	if(!a)return 0;//double-oh-seven
--- multiply/xorSwap.h
+++ multiply/xorSwap.h
@@ -1,3 +1,20 @@
+/*
+    Better way of multiplication
+    Copyright (C) 2011  David Ćavar
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef XORSWAP_H
 #define XORSWAP_H
 	void xorSwap(int *a,int *b);
